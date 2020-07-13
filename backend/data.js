833bcdbe7c2c68b1f8dbcfe772e5ba7d760e2283
Binay <binayprogramming@gmail.com>
2020-07-13 23:53:44 +0200

    Created node server for our backend, configured babel and its cli to work efficiently with ES6

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Slim Shirt",
+      catagory: "Shirts",
+      image: "/images/d1.jpg",
+      brand: "Nike",
+      price: 60,
+      rating: 4,
+      numReviews: 10,
+    },
+    {
+      _id: "2",
+      name: "Fit Shirt",
+      catagory: "Shirts",
+      image: "/images/d1.jpg",
+      brand: "Adidas",
+      price: 30,
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: "3",
+      name: "Pants",
+      catagory: "Pants",
+      image: "/images/d1.jpg",
+      brand: "Puma",
+      price: 80,
+      rating: 3,
+      numReviews: 10,
+    },
+  ],
+};
